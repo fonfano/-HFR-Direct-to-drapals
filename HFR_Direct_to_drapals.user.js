fa06efcc99bcbebb927c704e03692c79c59aff01
fonfano <fonfano@gmail.com>
2020-10-02 09:37:20 +0200

    Update HFR_Direct_to_drapals.user.js

--- HFR_Direct_to_drapals.user.js
+++ HFR_Direct_to_drapals.user.js
@@ -3,30 +3,33 @@
 // @namespace   github.com/fonfano
 // @match       https://forum.hardware.fr/*
 // @grant       none
-// @version     0.2.0
+// @version     0.2.1
 // @author      Lt Ripley
-// @description Charge automatiquement la page des drapeaux lorsqu'on va sur la racine du forum.hardware.fr
+// @description Charge automatiquement la page des drapeaux lorsqu'on va sur la racine forum.hardware.fr
 // ==/UserScript==
 
 
 // Historique
-// 28/09/2020   Refonte.    v 0.2.0 : Plus rapide.  Va directement sur les drapeaux sans passer par la racine qui en plus maintenant reste accessible sans désactiver le script
+// 02/10/2020   Modif       v 0.2.1 : Petite modification du code pour fonctionner aussi quand il n'y a qu'un seul arbre
+// 28/09/2020   Refonte.    v 0.2.0 : Plus rapide.  Va directement sur les drapeaux sans passer par la racine qui aussi maintenant reste accessible sans désactiver le script
 // 25/09/2020   Création.   v 0.1.0
 
 
 // Merci aux devs du topic Pimp my HFR !
 
 
-if ( document.getElementById("md_arbo_tree_1") && document.getElementById("md_arbo_tree_b_1") )   // double condition pour s'assurer qu'on est dans un topic
-                                                                                                  // et laisser l'accès a la racine quand on est pas dans un topic
-{
-  let upRootLink = document.getElementById("md_arbo_tree_1");     // lien vers la racine dans l'arbre du haut
-  let downRootLink = document.getElementById("md_arbo_tree_b_1"); // du bas
+if ( document.getElementById("md_arbo_tree_1") )  {
   
-  upRootLink.onclick = toDrapals;
-  downRootLink.onclick = toDrapals;
-}
+  let upperRootLink = document.getElementById("md_arbo_tree_1");     // lien vers la racine dans l'arbre du haut
+  upperRootLink.onclick = toDrapals;
+  }
 
+
+if ( document.getElementById("md_arbo_tree_b_1") )  {
+  
+  let lowerRootLink = document.getElementById("md_arbo_tree_b_1"); // du bas
+  lowerRootLink.onclick = toDrapals;
+  }
 
 function toDrapals() {
   
